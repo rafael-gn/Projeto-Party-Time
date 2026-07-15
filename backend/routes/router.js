38fa59660a36b6ce12799ace6b06fcf0a2c232c1
rafael-gn <rafael.gn.066@gmail.com>
2026-07-15 18:14:10 -0300

    Inserção/Resgate/Exlução/Atualização dos serviços no banco de dados

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -6,4 +6,9 @@ const servicesRouter = require("./services")
 
 router.use("/", servicesRouter)
 
+//Parties routes
+const partyRouter = require("./parties")
+
+router.use("/", partyRouter)
+
 module.exports = router
