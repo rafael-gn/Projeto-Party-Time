3e88af820d1067ea9ce33a9487970aed82ecb8b5
rafael-gn <rafael.gn.066@gmail.com>
2026-07-16 14:52:29 -0300

    inserção de festas/Trazendo os serviços para as festas/ CSS do Formulario

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,11 +1,85 @@
-import React from 'react'
+import partyFetch from "../axios/config";
+import { useState, useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import "./Form.css";
 
 const CreateParty = () => {
-  return (
-    <div>
-      <h1>CreateParty</h1>
-    </div>
-  )
-}
+  const [services, setServices] = useState([]);
 
-export default CreateParty
+  //Load services
+
+  useEffect(() => {
+    const loadServices = async () => {
+      const res = await partyFetch.get("/services");
+
+      setServices(res.data);
+    };
+
+    loadServices();
+  }, []);
+
+  return (
+    <div className="form-page">
+      <h2>Crie sua próxima festa</h2>
+      <p>Defina o seu orçamento e escolha os serviços</p>
+      <form>
+        <label>
+          <span>Nome da festa:</span>
+          <input type="text" placeholder="Seja criativo..." required />
+        </label>
+
+        <label>
+          <span>Anfitrião:</span>
+          <input type="text" placeholder="Quem esta dando a festa ?" required />
+        </label>
+
+        <label>
+          <span>Descrição:</span>
+          <textarea placeholder="Fale sobre a festa..." required></textarea>
+        </label>
+
+        <label>
+          <span>Orçamento</span>
+          <input
+            type="number"
+            placeholder="Quanto voce pretende investir?"
+            required
+          />
+        </label>
+
+        <label>
+          <span>Imagem:</span>
+          <input
+            type="text"
+            placehlder="inisira a URl de uma imagem"
+            required
+          />
+        </label>
+
+        <div className="">
+          <h2>Escolha os serviços</h2>
+          <div className="services-container">
+            {services.length === 0 && <p>Carregando...</p>}
+            {services.length > 0 &&
+              services.map((service) => (
+                <div className="service" key={service._id}>
+                  <img src={service.image} alt={service.name} />
+                  <p className="service-name">{service.name}</p>
+                  <p className="service-price">R${service.price}</p>
+                  <div className="checkbox-container">
+                    <input type="checkbox" value={service._id} />
+                    <p>Marque para solicitar</p>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+        <input type="submit" value="Criar Festa" className="btn" />
+      </form>
+    </div>
+  );
+};
+
+export default CreateParty;
